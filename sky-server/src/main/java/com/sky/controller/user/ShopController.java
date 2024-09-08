4d3b58a1c51a762728290f279f2624527efd8bd4
ZenSyntax <moreantx@gmail.com>
2024-09-08 15:08:09 +0800

    实现了店铺状态的更改和查询

---
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,31 @@
+package com.sky.controller.user;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("userShopController")//与admin的ShopController的bean区分开来，这样创建bean对象就不冲突了
+@RequestMapping("/user/shop")
+@Api(tags = "店铺相关接口")
+@Slf4j
+public class ShopController {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 获取店铺状态
+     * @return
+     */
+    @GetMapping("/status")
+    @ApiOperation("获取店铺营业状态")
+    public Result<Integer> getStatus() {
+        Integer shopStatus = (Integer) redisTemplate.opsForValue().get("shop_status");
+        log.info("设置店铺的营业状态为：{}", shopStatus == 1 ? "营业中" : "打烊中");
+        return Result.success(shopStatus);
+    }
+}
